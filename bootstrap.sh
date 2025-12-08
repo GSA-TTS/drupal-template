ba2230165969023637156bf7c681c8f3d1491a1c
Ryan Ahearn <ryan.ahearn@gsa.gov>
2025-12-08 09:29:28 -0500

    Remove need for local composer

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,20 +12,26 @@ if [ $# -lt 1 ]; then
   exit 1;
 fi
 
-if ! which composer > /dev/null; then
-  echo "composer is required. Install from homebrew"
-  exit 1
-fi
-
 if ! which ddev > /dev/null; then
   echo "ddev is required. Install from homebrew"
   exit 1
 fi
 
 output_dir="$1"
+template_dir="$(pwd)/$(dirname "$0")/templates"
 set -e
 
-composer create-project drupal/cms "$output_dir"
+if [ -d "$output_dir" ]; then
+  echo "Output directory $output_dir already exists"
+  exit 1
+fi
+
+mkdir -p "$output_dir"
+cd "$output_dir"
+
+ddev config --project-type=drupal --docroot=web
+ddev start
+ddev composer create-project drupal/cms
 
 echo
 echo "======================================================================"
@@ -34,15 +40,15 @@ echo "Copying config files"
 echo
 echo "======================================================================"
 echo
-cp templates/apt.yml "$output_dir"
-cp templates/gitignore "$output_dir"/.gitignore
-cp templates/init.sh "$output_dir"
-cp templates/settings*.php "$output_dir"/web/sites/default/
-mkdir -p "$output_dir"/.bp-config/php/php.ini.d
-cp templates/bp-config/options.json "$output_dir"/.bp-config/
-cp templates/bp-config/*.ini "$output_dir"/.bp-config/php/php.ini.d/
-cp -r templates/scripts "$output_dir"
-cp -r templates/terraform "$output_dir"
+cp "${template_dir}"/apt.yml .
+cp "${template_dir}"/gitignore .gitignore
+cp "${template_dir}"/init.sh .
+cp "${template_dir}"/settings*.php web/sites/default/
+mkdir -p .bp-config/php/php.ini.d
+cp "${template_dir}"/bp-config/options.json .bp-config/
+cp "${template_dir}"/bp-config/*.ini .bp-config/php/php.ini.d/
+cp -r "${template_dir}"/scripts .
+cp -r "${template_dir}"/terraform .
 
 
 echo
@@ -53,7 +59,6 @@ echo
 echo "======================================================================"
 echo
 
-cd "$output_dir"
 chmod +x init.sh
 
 echo
@@ -63,7 +68,7 @@ echo "Adding s3fs"
 echo
 echo "======================================================================"
 echo
-composer require drupal/s3fs
+ddev composer require drupal/s3fs
 
 echo
 echo "======================================================================"
@@ -73,6 +78,4 @@ echo
 echo "======================================================================"
 echo
 
-ddev config --project-type=drupal --docroot=web
-ddev start
-ddev ssh
+exec ddev ssh
